f7d3286999c94f5dc1b174933886477ca2999966
tdubourg <tdubourg@users.noreply.github.com>
2018-01-27 11:20:51 -0800

    Main stage update: bullet soldier and pigeon and camera and scripts attaches to them

--- unity_project/CherAmi/Assets/scirpts/environment.cs
+++ unity_project/CherAmi/Assets/scirpts/environment.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class environment : MonoBehaviour {
-
+    public const int SOLDIERS_SPAWN_INTERVAL_SEC = 5;
+    static List<soldier> soldiers = new List<soldier>();
+    float timeSinceLastSoldierSpawn = 0;
+    public int TryAttribute = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,58 @@ public class environment : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var dt = Time.deltaTime;
+        this.timeSinceLastSoldierSpawn += dt;
+        if (this.timeSinceLastSoldierSpawn > SOLDIERS_SPAWN_INTERVAL_SEC)
+        {
+            // spawn the soldier
+            Debug.Log("Yolo");
+        }
+    }
 }
+
+/*
+var Environment = pc.createScript('Environment');
+
+var soldiers = [];
+
+SOLDIERS_SPAWN_INTERVAL_SEC = 5; // interval in seconds, to spawn a soldier
+    
+// initialize code called once per entity
+Environment.prototype.initialize = function()
+{
+    this.timeSinceLastSpawn = 0;
+};
+
+// update code called every frame
+Environment.prototype.update = function(dt)
+{
+    // console.log("envUpd");
+
+    var player = this.entity; // This script is attached to the pigeon so this.entity points to the player
+                              // console.log("Current player position is", player.getPosition());
+                              // console.log("pcscriptcomponent", pc.ScriptComponent.scripts);
+                              // console.log(Pigeon);
+    this.genEnvironment(dt);
+
+};
+
+Environment.prototype.genEnvironment = function(dt)
+{
+    // console.log("genEnv");
+    // console.log("elapsedTime", Game.game.elapsedTime);
+    this.timeSinceLastSpawn += dt;
+    // console.log("Time since last soldier spawn:", this.timeSinceLastSpawn, dt);
+    if (this.timeSinceLastSpawn > SOLDIERS_SPAWN_INTERVAL_SEC)
+    {
+        console.log("Spawning a new soldier");
+        this.timeSinceLastSpawn = 0;
+        var newSoldier = this.app.root.findByName("soldier").clone();
+        var ppos = Pigeon.pigeon.getPosition();
+        var ground_pos = this.app.root.findByName("GROUND").getPosition();
+        console.log("minX", Pigeon.pigeon.attributes.get("minX"));
+        newSoldier.setPosition(ppos.x + 10, ground_pos.y, 0);
+        this.app.root.addChild(newSoldier);
+    }
+}
+*/
--- unity_project/CherAmi/Assets/scirpts/soldier.cs
+++ unity_project/CherAmi/Assets/scirpts/soldier.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Soldier : MonoBehaviour {
+public class soldier : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
