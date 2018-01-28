c2a5b97b0318ea955fb0b8279806637179db77a5
tdubourg <tdubourg@users.noreply.github.com>
2018-01-28 14:47:29 -0800

    falling feathers again

--- unity_project/CherAmi/Assets/scirpts/feathersFalling.cs
+++ unity_project/CherAmi/Assets/scirpts/feathersFalling.cs
@@ -11,6 +11,9 @@ public class feathersFalling : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        var dt = Time.deltaTime;
+        var speed = 0.5f;
+        var direction = new Vector3(0, -1, 0);
+        this.transform.Translate(direction * speed);
 	}
 }
